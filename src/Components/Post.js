3350b9e8871b172538e69520c3834a26258c9e20
Bilal Azam <bilalazam1678@gmail.com>
2024-06-24 01:50:23 +0300

    Postlist component is creaed and also some changes in home component even its name is been changed

---
+++ src/Components/Post.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { PostContext } from '../context/PostContext';
+
+function Post({ post }) {
+  const { deletePost } = useContext(PostContext);
+
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+      <button onClick={() => deletePost(post.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default Post;
---
+++ src/Components/PostList.js
@@ -0,0 +1,18 @@
+// src/components/PostList.js
+import React, { useContext } from 'react';
+import { PostContext } from '../context/PostContext';
+import Post from './Post';
+
+function PostList() {
+  const { posts } = useContext(PostContext);
+
+  return (
+    <div>
+      {posts.map(post => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
+
+export default PostList;
