b36c6a70d34f62e6a966ae97e5a601f4f10e0c2b
Bilal Azam <bilalazam1678@gmail.com>
2024-06-25 00:34:14 +0300

    New directory is maded named it Pages and new  function Home is maded

---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PostList from "../Components/PostList";
+
+
+function Home() {
+
+return(
+
+    <div>
+<h1>Blog Post</h1>
+<PostList />
+    </div>
+);
+
+
+}
+
+export default Home;
